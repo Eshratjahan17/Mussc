aa876bd01b26ed388581b30083369145e174829a
hasanAbdullah9825 <hasan.techtomorrow@gmail.com>
2023-03-21 11:32:56 +0600

    working on card

--- src/Components/Home/Events/Event.js
+++ src/Components/Home/Events/Event.js
@@ -1,27 +1,28 @@
 import React from "react";
-import { SwiperSlide } from "swiper/react";
+import './Event.css';
 
 const Event = ({ event }) => {
   const { title, picture, about } = event;
   console.log(title,picture);
   return (
-    <SwiperSlide>
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <figure>
+   
+    <div className="card w-96  bg-base-100 shadow-xl mt-7">
+      
         <img
+        className="h-60 card-img"
           src={picture}
           alt="Shoes"
         />
-      </figure>
+      
       <div className="card-body">
         <h2 className="card-title">{title}!</h2>
-        <p>{about}</p>
+        {/* <p>{about}</p> */}
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-primary">See More</button>
         </div>
       </div>
     </div>
-    </SwiperSlide>
+    
   );
 };
 
--- src/Components/Home/Events/Events.js
+++ src/Components/Home/Events/Events.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+
 import Event from "./Event";
 
 const Events = () => {
@@ -15,25 +14,16 @@ console.log(events)})
   return (
     <div className=" py-5 px-10">
       <h1 className="text-5xl font-bold text-center pb-6 ">
-        Our Events
+        Our Recent Events
       </h1>
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        
-         
+     
+        <div className="grid  lg:grid-cols-3 sm:grid-cols-1  justify-items-center">
+          
          {
           events.map((event)=><Event event={event}></Event>)
         }
-         
-       
-        
-       
-        ...
-      </Swiper>
+        </div>
+     
     </div>
   );
 };
